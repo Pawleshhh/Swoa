dfb5699edb88c87625cf3e28ededfac4b52b071e
Mateusz Pawlikowski <mateusz46668@gmail.com>
2021-09-23 11:43:17 +0200

    Add unit types.

---
+++ Astronomy/Units/UnitTypes/MassUnit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Astronomy.Units
+{
+    public sealed class MassUnit : Unit
+    {
+
+        private MassUnit(string unitName, string unitSignature)
+            : base(unitName, unitSignature) { }
+
+        public static MassUnit Kilogram { get; } = new MassUnit("Kilogram", "kg");
+        public static MassUnit Ton { get; } = new MassUnit("Ton", "t");
+
+    }
+}
