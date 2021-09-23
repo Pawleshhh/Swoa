d3379d16810229e1b00755e71d54ba9fb0d4c2d9
Mateusz Pawlikowski <mateusz46668@gmail.com>
2021-09-23 08:24:29 +0200

    Add ICelestialObject interface

---
+++ CelestialObjects/Interface/ICelestialObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CelestialObjects
+{
+    public interface ICelestialObject : IEquatable<ICelestialObject>
+    {
+
+        string Name { get; }
+
+        double VisualMagnitude { get; }
+
+    }
+}
