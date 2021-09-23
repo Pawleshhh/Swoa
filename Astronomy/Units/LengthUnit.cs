1f25028c4789bb57da9c53d65a588bddcdb56840
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2021-09-23 18:16:56 +0200

    Astronomy project changes. Add unit types.

---
+++ Astronomy/Units/LengthUnit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Astronomy.Units
+{
+    public sealed class LengthUnit : Unit
+    {
+
+        private LengthUnit(string unitName, string unitSignature)
+            : base(unitName, unitSignature) { }
+
+        public static LengthUnit Kilometer { get; } = new LengthUnit("Kilometer", "km");
+        public static LengthUnit Mile { get; } = new LengthUnit("Mile", "MI");
+
+    }
+}
