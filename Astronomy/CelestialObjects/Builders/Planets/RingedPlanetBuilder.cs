631e871cfee292122fbb4c7a02bc3ae9ed441b74
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2021-09-24 22:05:52 +0200

    Add RingedPlanetBuilder class.

---
+++ Astronomy/CelestialObjects/Builders/Planets/RingedPlanetBuilder.cs
@@ -0,0 +1,25 @@
+﻿using Astronomy.CelestialObjects.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities;
+
+namespace Astronomy.CelestialObjects
+{
+    public class RingedPlanetBuilder : PlanetObjectBuilder
+    {
+
+        public int RingCount { get; set; }
+
+        protected override Dictionary<string, IProperty> GetProperties()
+        {
+            var properties = base.GetProperties();
+            properties.AddKeyValuePair(AllProperties.RingCount.ToKeyValuePair(RingCount));
+
+            return properties;
+        }
+
+    }
+}
