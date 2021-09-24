31e3ff3c91492ac12269ec9c8017bed53a07f2d0
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2021-09-24 22:48:11 +0200

    Add DeepSkyObjectBuilder class.

---
+++ Astronomy/CelestialObjects/Builders/DeepSkyObjects/DeepSkyObjectBuilder.cs
@@ -0,0 +1,25 @@
+﻿using Astronomy.CelestialObjects.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Astronomy.CelestialObjects
+{
+    public class DeepSkyObjectBuilder : CelestialObjectBuilder
+    {
+
+        public string DeepSkyObjectType { get; set; } = string.Empty;
+
+        protected override Dictionary<string, IProperty> GetProperties()
+        {
+            var properties = new Dictionary<string, IProperty>(new KeyValuePair<string, IProperty>[]
+            {
+                AllProperties.DeepSkyObjectType.ToKeyValuePair(DeepSkyObjectType)
+            });
+
+            return properties;
+        }
+    }
+}
