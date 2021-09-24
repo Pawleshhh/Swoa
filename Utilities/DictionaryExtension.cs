910cc27585a5aa937d68776a8651d268cffd61ff
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2021-09-24 22:01:11 +0200

    Add PhasedPlanetBuilder class.

---
+++ Utilities/DictionaryExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public static class DictionaryExtension
+    {
+
+        public static void AddKeyValuePair<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, KeyValuePair<TKey, TValue> keyValuePair)
+        {
+            dictionary.Add(keyValuePair.Key, keyValuePair.Value);
+        }
+
+    }
+}
