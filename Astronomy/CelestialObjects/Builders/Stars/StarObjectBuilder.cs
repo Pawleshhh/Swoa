1c686ccb43dd044285a90cb74bf4888858b9cd1d
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2021-09-24 22:30:29 +0200

    Add StarObjectBuilder class.

---
+++ Astronomy/CelestialObjects/Builders/Stars/StarObjectBuilder.cs
@@ -0,0 +1,31 @@
+﻿using Astronomy.CelestialObjects.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Astronomy.CelestialObjects
+{
+    public class StarObjectBuilder : CelestialObjectBuilder
+    {
+
+        public int AbsoluteMagnitude { get; set; }
+
+        public string SpectralClass { get; set; } = string.Empty;
+
+        public double DistanceToSun { get; set; }
+
+        protected override Dictionary<string, IProperty> GetProperties()
+        {
+            var properties = new Dictionary<string, IProperty>(new KeyValuePair<string, IProperty>[]
+            {
+                AllProperties.AbsoluteMagnitude.ToKeyValuePair(AbsoluteMagnitude),
+                AllProperties.SpectralClass.ToKeyValuePair(SpectralClass),
+                AllProperties.DistanceToSun.ToKeyValuePair(DistanceToSun)
+            });
+
+            return properties;
+        }
+    }
+}
