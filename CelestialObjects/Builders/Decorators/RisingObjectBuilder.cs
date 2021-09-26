5cfaf3321d4b40800bd6baab0da9089fae52fede
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2021-09-26 18:25:53 +0200

    Add RisingObjectBuilder class.

---
+++ CelestialObjects/Builders/Decorators/RisingObjectBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities;
+
+namespace CelestialObjects
+{
+    public class RisingObjectBuilder : CelestialObjectBuilderDecorator
+    {
+        public RisingObjectBuilder(ICelestialObjectBuilder celestialObjectBuilder) : base(celestialObjectBuilder)
+        {
+        }
+
+        public TimeSpan RisesAt { get; set; }
+        public TimeSpan SetsAt { get; set; }
+
+        protected override Dictionary<string, IProperty> GetDecoratorProperties()
+        {
+            var properties = new Dictionary<string, IProperty>();
+            properties.AddKeyValuePair(CelestialObjectBuilder.GetKeyValuePairNormalized(nameof(RisesAt), RisesAt));
+            properties.AddKeyValuePair(CelestialObjectBuilder.GetKeyValuePairNormalized(nameof(SetsAt), SetsAt));
+            return properties;
+        }
+    }
+}
