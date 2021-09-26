8921a42a12b33526a7fe6d40ce0179d0a9ebeeeb
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2021-09-26 18:49:22 +0200

    Add PhasingObjectBuilder class.

---
+++ CelestialObjects/Builders/Decorators/PhasingObjectBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities;
+
+namespace CelestialObjects
+{
+    public class PhasingObjectBuilder : CelestialObjectBuilderDecorator
+    {
+        public PhasingObjectBuilder(ICelestialObjectBuilder celestialObjectBuilder) : base(celestialObjectBuilder)
+        {
+        }
+
+        public string Phase { get; set; } = string.Empty;
+        public double PhaseIllumination { get; set; }
+
+        protected override Dictionary<string, IProperty> GetDecoratorProperties()
+        {
+            var properties = new Dictionary<string, IProperty>();
+            properties.AddKeyValuePair(CelestialObjectBuilder.GetKeyValuePairNormalized(nameof(Phase), Phase));
+            properties.AddKeyValuePair(CelestialObjectBuilder.GetKeyValuePairNormalized(nameof(PhaseIllumination), PhaseIllumination));
+
+            return properties;
+        }
+    }
+}
