6ac52959f596c71c12b7f29c8e96a28555c2413e
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2021-09-26 18:39:44 +0200

    Add RotatingObjectBuilder class.

---
+++ CelestialObjects/Builders/Decorators/RotatingObjectBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities;
+
+namespace CelestialObjects
+{
+    public class RotatingObjectBuilder : CelestialObjectBuilderDecorator
+    {
+        public RotatingObjectBuilder(ICelestialObjectBuilder celestialObjectBuilder) : base(celestialObjectBuilder)
+        {
+        }
+
+        public TimeSpan LengthOfDay { get; set; }
+
+        protected override Dictionary<string, IProperty> GetDecoratorProperties()
+        {
+            var properties = new Dictionary<string, IProperty>();
+            properties.AddKeyValuePair(CelestialObjectBuilder.GetKeyValuePairNormalized(nameof(LengthOfDay), LengthOfDay));
+
+            return properties;
+        }
+    }
+}
