97009c94cdd1e8018181a4265e76be71965f6fda
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2021-10-12 20:27:49 +0200

    Add Multiply and Divide Value Converters. CelestalMap in MainWindow keeps its square proportions.

---
+++ Swoa.UI/Converters/DivideValueConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Swoa.UI
+{
+    public class DivideValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double dividend = (double)value;
+            double divisor = double.Parse((string)parameter);
+
+            return dividend / divisor;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double dividend = (double)value;
+            double divisor = double.Parse((string)parameter);
+
+            return dividend * divisor;
+        }
+    }
+}
