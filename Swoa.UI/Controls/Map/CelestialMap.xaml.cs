659fb7d2bafd1913582a5db37e2171247c8b2782
Mateusz Pawlikowski <mateusz46668@gmail.com>
2021-10-13 09:20:08 +0200

    Add zooming in/out for Celstial Map

--- Swoa.UI/Controls/Map/CelestialMap.xaml.cs
+++ Swoa.UI/Controls/Map/CelestialMap.xaml.cs
@@ -36,6 +36,45 @@ namespace Swoa.UI
         public static readonly DependencyProperty AngleProperty =
             DependencyProperty.Register("Angle", typeof(double), typeof(CelestialMap));
 
+        public double ScaleFactorStep
+        {
+            get { return (double)GetValue(ScaleFactorStepProperty); }
+            set { SetValue(ScaleFactorStepProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ScaleFactorStep.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ScaleFactorStepProperty =
+            DependencyProperty.Register("ScaleFactorStep", typeof(double), typeof(CelestialMap), new PropertyMetadata(0.1));
+
+        public double ScaleFactor
+        {
+            get { return (double)GetValue(ScaleFactorProperty); }
+            set { SetValue(ScaleFactorProperty, value); }
+        }
+
+        public static readonly DependencyProperty ScaleFactorProperty = DependencyProperty.Register("ScaleFactor", typeof(double), 
+            typeof(CelestialMap), new FrameworkPropertyMetadata(1.0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public double MaxScaleFactor
+        {
+            get { return (double)GetValue(MaxScaleFactorProperty); }
+            set { SetValue(MaxScaleFactorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MaxScaleFactor.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MaxScaleFactorProperty =
+            DependencyProperty.Register("MaxScaleFactor", typeof(double), typeof(CelestialMap), new PropertyMetadata(3.0));
+
+        public double MinScaleFactor
+        {
+            get { return (double)GetValue(MinScaleFactorProperty); }
+            set { SetValue(MinScaleFactorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MinScaleFactor.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MinScaleFactorProperty =
+            DependencyProperty.Register("MinScaleFactor", typeof(double), typeof(CelestialMap), new PropertyMetadata(1.0));
+
         public CelestialMap()
         {
             InitializeComponent();
@@ -43,6 +82,39 @@ namespace Swoa.UI
             MouseLeftButtonDown += OnMouseLeftButtonDown;
             MouseUp += OnMouseUp;
             MouseMove += OnMouseMove;
+
+            MouseWheel += CelestialMap_MouseWheel;
+        }
+
+        private void CelestialMap_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            double d = e.Delta;
+
+            if (ScaleFactor >= MaxScaleFactor && d > 0)
+            {
+                ScaleFactor = MaxScaleFactor;
+                return;
+            }
+
+            if (ScaleFactor < MinScaleFactor)
+            {
+                ScaleFactor = MinScaleFactor;
+                RenderTransformOrigin = new Point(0.5, 0.5);
+                return;
+            }    
+
+
+            if (ScaleFactor == MinScaleFactor && d < 0)
+                return;
+
+            var currentPosition = Mouse.GetPosition(this);
+
+            RenderTransformOrigin = new Point(currentPosition.X / ActualHeight, currentPosition.Y / ActualWidth);
+
+            if (d > 0)
+                ScaleFactor += ScaleFactorStep;
+            else
+                ScaleFactor -= ScaleFactorStep;
         }
 
         private void OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -71,7 +143,7 @@ namespace Swoa.UI
                 // Calculate an angle
                 double radians = Math.Atan((currentLocation.Y - mapCenter.Y) /
                                            (currentLocation.X - mapCenter.X));
-                Angle = (radians * 180 / Math.PI);
+                Angle = (radians * 180 / Math.PI) - 180.0;
 
                 // Apply a 180 degree shift when X is negative so that we can rotate
                 // all of the way around
