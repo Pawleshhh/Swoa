9260ac796b84b1a7e9a2bf46e32f88d81110d3a6
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2021-11-08 20:19:53 +0100

    Button time direction backgrounds change in regard to current direction.

---
+++ Swoa.UI/Converters/BooleanNegationConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Swoa.UI
+{
+    public class BooleanNegationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool b_value = (bool)value;
+            return !b_value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool b_value = (bool)value;
+            return !b_value;
+        }
+    }
+}
